aaeaa6030f1164f041b1dc1bf08b3fdb43f74495
anurajguha <anurajguha@gmail.com>
2022-09-08 22:09:30 +0530

    update message structure to be queued

--- src/api/controllers/message.controller.js
+++ src/api/controllers/message.controller.js
@@ -1,4 +1,5 @@
 const httpStatus = require('http-status');
+const { v4: uuid } = require('uuid');
 const { sendMessageToQueue } = require('../../config/rabbitmq');
 
 
@@ -7,7 +8,12 @@ const sendMessage = async (req, res, next) => {
     console.log('inside send message controller');
     try {
         const { message } = req.body;
-        sendMessageToQueue(message);
+        const queueData = {
+            message,
+            messageId: uuid(),
+            receivedAt: Date.now()
+        };
+        sendMessageToQueue(queueData);
         res.status(httpStatus.OK).send({ status: true, message: 'message delivered' });
 
     } catch (error) {
